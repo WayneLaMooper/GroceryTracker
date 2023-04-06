2dae9e33d613f990c6f7267000966dd184c8781b
WayneLaMooper <waynela162002@gmail.com>
2023-04-05 19:48:22 -0600

    Created login, and signup page php files

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
+<?php
+session_start();
+
+$_SESSION;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <link rel="stylesheet" href="index.css" />
-    <title>My first web page</title>
-  </head>
-  <body>
-    <img src="images/download.jpg" />
-    <p class="username">@Mooper</p>
-    <p>I love yoooou!</p>
-    <p> hello again</p>
-  </body>
+
+<head>
+  <link rel="stylesheet" href="index.css" />
+  <title>Welcome to Grocery Tracker!</title>
+</head>
+
+<body>
+
+  <a href="logout.php">Logout</a>
+  <h1>This is the index page</h1>
+
+  <br>
+  Hello, Username.
+</body>
+
 </html>
