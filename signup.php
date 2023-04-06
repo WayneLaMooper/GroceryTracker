5ef16547dfeec85bab885da6729ee4c59774cfb7
WayneLaMooper <waynela162002@gmail.com>
2023-04-05 21:17:39 -0600

    Signup successfully adds information to database

--- signup.php
+++ signup.php
@@ -4,6 +4,22 @@ session_start();
 include("connection.php");
 include("functions.php");
 
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $user_name = $_POST['user_name'];
+    $password = $_POST['password'];
+
+    if (!empty($user_name) && !empty($password)) {
+        $user_id = random_num(20);
+        $query = "insert into user_account (account_ID, username, password) values ('$user_id', '$user_name', '$password')";
+
+        mysqli_query($con, $query);
+        header("Location: login.php");
+        die;
+    } else {
+        echo "Please enter valid information";
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
