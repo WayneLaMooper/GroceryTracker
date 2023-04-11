7f47ac484d6f8423a83d6b398dcba7436b8e60c2
JohnLugue <lugue.johndale@gmail.com>
2023-04-10 20:22:22 -0600

    adding images and search bar (no functionality)

--- mainpage.php
+++ mainpage.php
@@ -4,11 +4,11 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="mainpage.css" />
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mainpage.css">
     <title>Grocery Tracker</title>
     <link rel="icon" href="images/icon.jpg" type="image/x-icon">
 </head>
@@ -16,8 +16,28 @@
     <h1> Grocery Tracker </h1>
 
     <!-- search function here -->
+    <div id = "search">
+        <form method="post">
+            <div>Search:</div> 
+            <input type="text" name="item"><br>
+        </form>
+    </div>
 
-    <!-- pictures on a gridlike fashion. click on image for a popup-->
+    <!-- pictures on a gridlike fashion. click on image for a popup -->
     
+    <div class ="row">
+        <div class = "column">
+            <img src="images/apple.jpg">
+            <img src="images/orange.jpg">
+            <img src="images/banana.jpg">
+        </div>
+
+        <div class = "column">
+            <img src="images/flour.jpg">
+            <img src="images/eggs.jpg">
+            <img src="images/butter.jpg">
+        </div>
+    </div>
+
 </body>
 </html>
