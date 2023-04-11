d6eaceb202e52de5f3dc2434b45e26879941b13d
JohnLugue <lugue.johndale@gmail.com>
2023-04-10 22:44:24 -0600

    removed mainpage

--- index.php
+++ index.php
@@ -24,7 +24,45 @@ $user_data = check_login($con);
   <h1>This is the index page </h1>
 
   <br>
-  Hello Username.
+
+    <!-- search function here. search an item for a popup?-->
+    <hr>
+    <div id = "search">
+        <form method="post">
+            <div>Search:</div> 
+            <input type="text" name="item"><br>
+        </form>
+    </div>
+
+    <!-- pictures on a gridlike fashion. click on image for a popup -->
+    <hr>
+    <div class ="gallery">
+        <figure>
+            <img src="images/apple.jpg">
+        </figure>
+            
+        <figure>
+            <img src="images/orange.jpg">
+        </figure>
+
+        <figure>
+            <img src="images/banana.jpg">
+        </figure>
+
+        <figure>
+            <img src="images/flour.jpg">
+        </figure>
+
+        <figure>
+            <img src="images/eggs.jpg">
+        </figure>
+
+        <figure>
+            <img src="images/butter.jpg">
+        </figure>
+
+
+    </div>
 </body>
 
 </html>
