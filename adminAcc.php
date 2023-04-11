d53c15587ff3cb30c71961df6776a07cb081605c
JohnLugue <lugue.johndale@gmail.com>
2023-04-10 19:51:50 -0600

    Added some essentials

--- adminAcc.php
+++ adminAcc.php
@@ -4,20 +4,14 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="mainpage.css" />
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Grocery Tracker</title>
+    <title>Admin Account</title>
     <link rel="icon" href="images/icon.jpg" type="image/x-icon">
 </head>
 <body>
-    <h1> Grocery Tracker </h1>
-
-    <!-- search function here -->
-
-    <!-- pictures on a gridlike fashion. click on image for a popup-->
     
 </body>
 </html>
