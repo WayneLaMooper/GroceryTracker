d2c7e978cab7a7ab07e025699ea8dc11d536bd08
guagyx1 <guagyx1@gmail.com>
2023-04-11 19:38:04 -0600

    store and product pages

---
+++ product.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+
+include("connection.php");
+include("functions.php");
+
+$user_data = check_login($con);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="index.css" />
+<link rel="icon" href="images/icon.jpg" type="image/x-icon">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to Grocery Tracker!</title>
+</head>
+
+<body>
+
+    <a href="index.php">back to main page</a>
+    <h1>This is the product page </h1>
+
+    <br>
+    <hr>
+
+    <!-- pictures on a gridlike fashion. click on image for a popup -->
+    <hr>
+    <div class="gallery">
+        <figure>
+            <img src="images/apple.jpg">
+        </figure>
+    </div>
+</body>
+
+</html>
