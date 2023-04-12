ce6a1749cc630dd834571d792dbd6c7d9ac23780
WayneLaMooper <waynela162002@gmail.com>
2023-04-12 03:41:48 -0600

    DIsplays unassigned managers in store

--- adminstore.php
+++ adminstore.php
@@ -17,7 +17,7 @@ $current_name = $store_info['store_name'];
 
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="index.css" />
+<link rel="stylesheet" href="adminstore.css" />
 <link rel="icon" href="images/icon.jpg" type="image/x-icon">
 
 <head>
@@ -26,88 +26,100 @@ $current_name = $store_info['store_name'];
 </head>
 
 <body>
+    <div>
+        <button class="return-button" onclick="location.href='ownerindex.php';">
+            Return to all Stores
+        </button>
 
-    <button class="return-button" onclick="location.href='ownerindex.php';">
-        Return to all Stores
-    </button>
+        <h1> Welcome back <?php echo $user_data['username']; ?> to <?php echo $current_name; ?>!</h1>
 
-    <h1> Welcome back <?php echo $user_data['username']; ?> to <?php echo $current_name; ?>!
-    </h1>
+        <form method="post">
+            <div>Add New Department!</div>
+            <br>
+            <div>Department Name:</div>
+            <input type="text" name="d_name"><br>
+            <br>
+            <input type="submit" value="Add Department"><br>
+        </form>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            if (isset($_POST['d_name'])) {
+                $dept_name = $_POST['d_name'];
 
-    <body>
-        <div id="box">
-            <form method="post">
-                <div>Add New Department!</div>
-                <br>
-                <div>Department Name:</div>
-                <input type="text" name="d_name"><br>
-                <br>
-                <input type="submit" value="Add Department"><br>
-            </form>
-            <?php
-            if ($_SERVER['REQUEST_METHOD'] == "POST") {
-                if (isset($_POST['d_name'])) {
-                    $dept_name = $_POST['d_name'];
+                if (!empty($dept_name)) {
+                    $queryName = "select * from department where shop_location = '$current_store' and dept_name = '$dept_name'";
+                    $result = mysqli_query($con, $queryName);
 
-                    if (!empty($dept_name)) {
-                        $queryName = "select * from department where shop_location = '$current_store' and dept_name = '$dept_name'";
-                        $result = mysqli_query($con, $queryName);
-
-                        if ($result && mysqli_num_rows($result) > 0) {
-                            echo "Department already exists.";
-                        } else {
-                            $query = "insert into department (dept_name, shop_location) values ('$dept_name', '$current_store')";
-                            mysqli_query($con, $query);
-                            echo "Department successfully added!";
-                        }
+                    if ($result && mysqli_num_rows($result) > 0) {
+                        echo "Department already exists.";
                     } else {
-                        echo "Empty entry, please try again.";
+                        $query = "insert into department (dept_name, shop_location) values ('$dept_name', '$current_store')";
+                        mysqli_query($con, $query);
+                        echo "Department successfully added!";
                     }
+                } else {
+                    echo "Empty entry, please try again.";
                 }
             }
-            ?>
+        }
+        ?>
+        <br>
+        <br>
+        <form method="post">
+            <div>Manager Account Creation!</div>
             <br>
+            <div>Username:</div>
+            <input type="text" name="m_username"><br>
             <br>
-            <form method="post">
-                <div>Manager Account Creation!</div>
-                <br>
-                <div>Username:</div>
-                <input type="text" name="m_username"><br>
-                <br>
-                <div>Password:</div>
-                <input type="password" name="m_password"><br>
-                <br>
-                <input type="submit" value="Create Account"><br>
-            </form>
-            <?php
-            if ($_SERVER['REQUEST_METHOD'] == "POST") {
-                if (isset($_POST['m_username'])) {
-                    $user_name = $_POST['m_username'];
-                    $password = $_POST['m_password'];
+            <div>Password:</div>
+            <input type="password" name="m_password"><br>
+            <br>
+            <input type="submit" value="Create Account"><br>
+        </form>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            if (isset($_POST['m_username'])) {
+                $user_name = $_POST['m_username'];
+                $password = $_POST['m_password'];
 
-                    if (!empty($user_name) && !empty($password)) {
-                        $user_id = random_num(20);
-                        $queryName = "select * from user_account where username = '$user_name'";
-                        $result = mysqli_query($con, $queryName);
+                if (!empty($user_name) && !empty($password)) {
+                    $user_id = random_num(20);
+                    $queryName = "select * from user_account where username = '$user_name'";
+                    $result = mysqli_query($con, $queryName);
 
-                        if ($result && mysqli_num_rows($result) > 0) {
-                            echo "Username taken, please try again.";
-                        } else {
-                            $query = "insert into user_account (account_ID, username, password) values ('$user_id', '$user_name', '$password')";
-                            mysqli_query($con, $query);
-                            $query = "insert into admin_acc (admin_ID) values ('$user_id')";
-                            mysqli_query($con, $query);
-                            $query = "insert into manager_acc (adminm_account_ID, shop_location) values ('$user_id', '$current_store')";
-                            mysqli_query($con, $query);
-                            echo "Account successfully created!";
-                        }
+                    if ($result && mysqli_num_rows($result) > 0) {
+                        echo "Username taken, please try again.";
                     } else {
-                        echo "Empty entry, please try again.";
+                        $query = "insert into user_account (account_ID, username, password) values ('$user_id', '$user_name', '$password')";
+                        mysqli_query($con, $query);
+                        $query = "insert into admin_acc (admin_ID) values ('$user_id')";
+                        mysqli_query($con, $query);
+                        $query = "insert into manager_acc (adminm_account_ID, shop_location) values ('$user_id', '$current_store')";
+                        mysqli_query($con, $query);
+                        echo "Account successfully created!";
                     }
+                } else {
+                    echo "Empty entry, please try again.";
                 }
             }
-            ?>
-        </div>
-    </body>
+        }
+        ?>
+    </div>
+    <div>
+        <h1>Unassigned Managers:</h1>
+        <?php
+        $freeManagers = "select * from manager_acc where shop_location = '$current_store' and dept_ID is NULL";
+        $result = mysqli_query($con, $freeManagers);
+        if ($result && mysqli_num_rows($result) > 0) {
+            while ($manager = mysqli_fetch_assoc($result)) {
+                $manQuery = "select username from user_account where account_ID = $manager[adminm_account_ID]";
+                $manager_result = mysqli_query($con, $manQuery);
+                $manager_info = mysqli_fetch_assoc($manager_result);
+                echo $manager_info['username'] . " " . $manager['adminm_account_ID'] . "<br>";
+            }
+        }
+        ?>
+    </div>
+</body>
 
 </html>
